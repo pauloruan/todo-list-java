401422b8acf77161c89610e28e65920cb46aa8cd
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2022-12-09 21:26:33 -0300

    feat: Repository `TaskRepository` created

--- src/main/java/com/pr/todolistjava/repository/TaskRepository.java
+++ src/main/java/com/pr/todolistjava/repository/TaskRepository.java
@@ -1,4 +1,9 @@
 package com.pr.todolistjava.repository;
 
-public interface TaskRepository {
+import com.pr.todolistjava.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
 }
